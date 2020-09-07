65ed8153220f8d97a09bd46eb3fbf9545cf913e5
PXshadow <joeyprather8@gmail.com>
2020-09-07 16:51:26 -0700

    clean up java

--- tests.sh
+++ tests.sh
@@ -1,7 +1,8 @@
-haxelib git hxjava https://github.com/HaxeFoundation/hxjava
+#haxelib git hxjava https://github.com/HaxeFoundation/hxjava
+#haxelib git hxcpp https://lib.haxe.org/p/hxcpp
 haxe tests.hxml
 neko test.n
 hl test.hl
 #./bin/cpp/Test.exe
-./bin/cs/bin/Test.exe
+#./bin/cs/bin/Test.exe
 sleep 10
