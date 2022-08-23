d69ab762d7fb9dee217c153d58f09c58d7641107
Neokeld <arnaud.duforat@protonmail.ch>
2022-08-24 01:17:03 +0200

    HS256 Signature

---
+++ tests/security/TestSign.hx
@@ -0,0 +1,39 @@
+package tests.security;
+
+import weblink.security.Sign;
+
+class TestSign {
+	private static function testHMAC256() {
+		var payload = "data";
+		var signature = Sign.sign(payload, "secret", Sign.HASH_METHOD);
+		var isOk = Sign.verify(payload, signature, "secret", Sign.HASH_METHOD);
+		if (!isOk) {
+			trace("Error");
+		}
+	}
+
+	private static function test_wrong_key() {
+		var payload = "data";
+		var signature = Sign.sign(payload, "secret", Sign.HASH_METHOD);
+		var isOk = Sign.verify(payload, signature, "another", Sign.HASH_METHOD);
+		if (isOk) {
+			trace("Error");
+		}
+	}
+
+	private static function test_unsupported_alg() {
+		var payload = "data";
+		try {
+			Sign.sign(payload, "secret", "SOMETHING");
+		} catch (e) {
+			return;
+		}
+		trace("Error");
+	}
+
+	public static function main() {
+		testHMAC256();
+		test_wrong_key();
+		test_unsupported_alg();
+	}
+}
---
+++ weblink/security/Sign.hx
@@ -0,0 +1,42 @@
+package weblink.security;
+
+import haxe.Exception;
+import haxe.crypto.Hmac;
+import haxe.io.Bytes;
+
+using StringTools;
+
+/*
+	HS256 signature
+ */
+class Sign {
+	public static var HASH_METHOD = "HS256";
+
+	public static function sign(message:String, priv_key:String, hash_method:String):String {
+		if (hash_method != HASH_METHOD) {
+			throw new Exception('$HASH_METHOD is the only supported algorithm for now (input was $hash_method)');
+		}
+		return sign_hash(message, priv_key, hash_method); // HMAC(key, SHA256(message))
+	}
+
+	private static function sign_hash(message:String, priv_key:String, hash_method:String):String {
+		// https://www.rfc-editor.org/rfc/rfc2104.html
+		var hmac:Hmac = new Hmac(HashMethod.SHA256);
+		var sb:Bytes = hmac.make(Bytes.ofString(priv_key), Bytes.ofString(message));
+		return Base64Url.encode(sb);
+	}
+
+	public static function verify(header_and_payload:String, signature:String, SECRET_KEY:String, ALGORITHM:String) {
+		var ok_signature = sign(header_and_payload, SECRET_KEY, ALGORITHM);
+		return fast_string_compare(ok_signature, signature);
+	}
+
+	// See: https://github.com/HaxeFoundation/hashlink/issues/176
+	public static function fast_string_compare(string1:String, string2:String):Bool {
+		if (string1.length != string2.length) {
+			return false;
+		}
+		var v = @:privateAccess string1.bytes.compare16(string2.bytes, string1.length);
+		return v == 0;
+	}
+}
