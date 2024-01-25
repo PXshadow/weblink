912e80433866e9dc52477c1caeaf0185898c98f8
Frixuu <kontakt@lukasz.xyz>
2024-01-25 05:25:57 +0100

    Allow insertion of named params to the routing tree

--- tests/TestRadixTree.hx
+++ tests/TestRadixTree.hx
@@ -1,8 +1,15 @@
+import haxe.EnumTools.EnumValueTools;
 import weblink._internal.ds.RadixTree;
 import weblink._internal.ds.RadixTreeUtils.commonPrefixLength;
 
 function assertEquals<T>(expected:T, actual:T) {
 	if (expected != actual) {
+		throw 'assert failed: expected "${expected}", actual "${actual}"';
+	}
+}
+
+function assertEqualEnum<T:EnumValue>(expected:T, actual:T) {
+	if (!EnumValueTools.equals(expected, actual)) {
 		throw 'assert failed: expected "${expected}", actual "${actual}"';
 	}
 }
@@ -25,31 +32,31 @@ class TestRadixTree {
 		}
 
 		{
-			final node = new Node("foobar", 4);
-			assertEquals("foobar", node.prefix);
+			final node = new Node(Literal("foobar"), 4);
+			assertEqualEnum(Literal("foobar"), node.edge);
 			assertEquals(4, node.value);
 			assertEquals(true, node.isLeaf());
 
 			node.splitEdge(3);
-			assertEquals("foo", node.prefix);
+			assertEqualEnum(Literal("foo"), node.edge);
 			assertEquals(null, node.value);
 			assertEquals(false, node.isLeaf());
 
 			final child = node.children[0];
-			assertEquals("bar", child.prefix);
+			assertEqualEnum(Literal("bar"), child.edge);
 			assertEquals(4, child.value);
 			assertEquals(true, child.isLeaf());
 		}
 
 		{
-			final node = new Node("foobar", 4);
+			final node = new Node(Literal("foobar"), 4);
 			node.splitEdge(0);
-			assertEquals("", node.prefix);
+			assertEqualEnum(Literal(""), node.edge);
 			assertEquals(null, node.value);
 			assertEquals(false, node.isLeaf());
 
 			final child = node.children[0];
-			assertEquals("foobar", child.prefix);
+			assertEqualEnum(Literal("foobar"), child.edge);
 			assertEquals(4, child.value);
 			assertEquals(true, child.isLeaf());
 		}
@@ -63,21 +70,30 @@ class TestRadixTree {
 			tree.put("banana", 5);
 
 			final root = tree.root;
-			assertEquals("", root.prefix);
+			assertEqualEnum(Literal(""), root.edge);
 			assertEquals(null, root.value);
 			assertEquals(2, root.children.length);
 
 			final app = root.children[0];
-			assertEquals("app", app.prefix);
+			assertEqualEnum(Literal("app"), app.edge);
 			assertEquals(3, app.value);
 			assertEquals(2, app.children.length);
 			assertEquals(3, app.children[0].value + app.children[1].value);
 
 			final ban = root.children[1];
-			assertEquals("ban", ban.prefix);
+			assertEqualEnum(Literal("ban"), ban.edge);
 			assertEquals(4, ban.value);
 			assertEquals(1, ban.children.length);
-			assertEquals("ana", ban.children[0].prefix);
+			assertEqualEnum(Literal("ana"), ban.children[0].edge);
+		}
+
+		{
+			final tree = new RadixTree<Int>();
+			tree.put("/a", 1);
+			tree.put("/a/:foo", 2);
+			tree.put("/b/:bar/:baz/quox", 3);
+			tree.put("/b/:bar", 4);
+			tree.put("/b/:bar/:baz/", 5);
 		}
 
 		trace("done");
--- weblink/_internal/ds/RadixTree.hx
+++ weblink/_internal/ds/RadixTree.hx
@@ -1,6 +1,8 @@
 package weblink._internal.ds;
 
-import weblink._internal.ds.RadixTreeUtils.commonPrefixLength;
+import weblink._internal.ds.RadixTreeUtils.*;
+
+using StringTools;
 
 class RadixTree<V> {
 	public var root:Node<V>;
@@ -14,14 +16,24 @@ class RadixTree<V> {
 	}
 }
 
+enum Edge {
+	Literal(path:String);
+	NamedParam(name:String);
+}
+
 @:allow(weblink._internal.ds.RadixTree)
 class Node<V> {
-	public var prefix:String;
+	public var parent:Null<Node<V>>;
+	public var edge:Edge;
 	public var value:Null<V>;
 	public var children:Null<Array<Node<V>>>;
 
-	public function new(prefix:String = "", ?value:V) {
-		this.prefix = prefix;
+	public function new(?edge:Edge, ?value:V) {
+		this.parent = null;
+		if (edge == null) {
+			edge = Literal("");
+		}
+		this.edge = edge;
 		this.value = value;
 		this.children = null;
 	}
@@ -31,8 +43,8 @@ class Node<V> {
 
 		(Subclasses should override this method if they have custom state.)
 	**/
-	private function create(prefix:String = "", ?value:V):Node<V> {
-		return new Node(prefix, value);
+	private function create(?edge:Edge, ?value:V):Node<V> {
+		return new Node(edge, value);
 	}
 
 	private function addChild(child:Node<V>) {
@@ -40,62 +52,153 @@ class Node<V> {
 			this.children = [];
 		}
 
+		child.parent = this;
 		this.children.push(child);
+		this.children.sort((a, b) -> switch [a.edge, b.edge] {
+			case [Literal(pa), Literal(pb)]: pa < pb ? -1 : (pa > pb ? 1 : 0);
+			case [Literal(_), NamedParam(_)]: -1;
+			case [NamedParam(_), Literal(_)]: 1;
+			case _: 0;
+		});
 	}
 
-	private function put(prefix:String, value:V) {
-		if (this.prefix == "" && this.isLeaf()) {
-			this.prefix = prefix;
-			this.value = value;
-			return;
-		}
+	private function describeEdge():String {
+		return switch (this.edge) {
+			case Literal(l): l;
+			case NamedParam(n): ':$n';
+		};
+	}
 
-		final cpl = commonPrefixLength(prefix, this.prefix);
-		if (cpl < this.prefix.length) {
-			this.splitEdge(cpl);
-		}
+	private function getPath():String {
+		var path = (this.parent != null) ? parent.getPath() : "";
+		path += this.describeEdge();
+		return path;
+	}
 
-		if (cpl < prefix.length) {
-			final nextPrefix = prefix.substring(cpl);
-			final nextChar = nextPrefix.charAt(0);
+	private function getLiteralPrefixOrThrow():String {
+		return switch this.edge {
+			case Literal(p): p;
+			case _: throw "node is not a Literal";
+		};
+	}
 
-			// Is there any existing child that we can share the prefix with?
-			if (!this.isLeaf()) {
-				for (child in this.children) {
-					if (nextChar == child.prefix.charAt(0)) {
-						// Yes, there is one! Push value recursively
-						child.put(nextPrefix, value);
-						return;
-					}
-				}
-			}
-
-			// No, there is not. Let's create a new one
-			final child = this.create(nextPrefix, value);
-			this.addChild(child);
-			return;
-		}
-
+	private function putHere(value:V) {
 		this.value = value;
 	}
 
-	public function splitEdge(pos:Int):Bool {
-		// Ensure pos is in range to prevent substring() failure
-		final prefix = this.prefix;
-		if (pos >= prefix.length || pos < 0) {
-			return false;
+	private function put(prefix:String, value:V) {
+		// Are we done by any chance?
+		if (prefix == "") {
+			this.putHere(value);
+			return;
 		}
 
-		// Move all data about self to a child node,
+		switch (this.edge) {
+			case Literal(""):
+				if (this.isLeaf()) {
+					if (this.value != null) {
+						throw 'invalid state, node with no path nor children somehow has value "${this.value}"';
+					}
+					final pair = parseEdgeUntilTypeChange(prefix);
+					this.edge = pair.left;
+					this.put(pair.right, value);
+				} else {
+					this.putIntoChildren(prefix, value);
+				}
+			case Literal(nodePath):
+				if (isNonLiteralStart(prefix)) {
+					// If the next path segment will not be a literal,
+					// checking common prefix will have no use
+					this.putIntoChildren(prefix, value);
+					return;
+				}
+
+				final divergePos = commonPrefixLength(nodePath, prefix);
+				if (divergePos < nodePath.length) {
+					this.splitEdge(divergePos);
+				}
+
+				if (divergePos == prefix.length) {
+					this.putHere(value);
+				} else {
+					this.putIntoChildren(prefix.substring(divergePos), value);
+				}
+			case NamedParam(paramName):
+				final pair = parseEdgeUntilTypeChange(prefix);
+				switch (pair.left) {
+					case NamedParam(newName):
+						if (newName != paramName) {
+							throw 'param named "$newName" conflicts with param "$paramName" on the same route';
+						}
+						if (pair.right == "") {
+							this.putHere(value);
+						} else {
+							this.putIntoChildren(pair.right, value);
+						}
+					case _:
+						this.putIntoChildren(prefix, value);
+				}
+		}
+	}
+
+	/**
+		Tries to find a child that can house our new value.
+		If no suitable child exists, creates a new one.
+	**/
+	private function putIntoChildren(prefix:String, value:V) {
+		final pair = parseEdgeUntilTypeChange(prefix);
+
+		// Are there any children that can take our value?
+		if (!this.isLeaf()) {
+			switch (pair.left) {
+				case Literal(path):
+					final nextChar = path.charAt(0);
+					for (child in this.children) {
+						switch (child.edge) {
+							case Literal(childPath):
+								if (nextChar == childPath.charAt(0)) {
+									child.put(prefix, value);
+									return;
+								}
+							case _:
+						}
+					}
+				case NamedParam(_):
+					for (child in this.children) {
+						if (child.edge.match(NamedParam(_))) {
+							child.put(pair.right, value);
+							return;
+						}
+					}
+			}
+		}
+
+		// No? Well, we tried everything. Just add a new child to the tree
+		final child = this.create(pair.left);
+		this.addChild(child);
+		child.put(pair.right, value);
+	}
+
+	public function splitEdge(pos:Int) {
+		final prefix = getLiteralPrefixOrThrow();
+
+		// Ensure pos is in range to prevent substring() failure
+		if (pos >= prefix.length || pos < 0) {
+			return;
+		}
+
 		final child = this.clone();
-		// except the prefix, which is now the right side of the cut
-		child.prefix = prefix.substring(pos);
+		child.edge = Literal(prefix.substring(pos));
+		child.parent = this;
+		if (!child.isLeaf()) {
+			for (grandchild in child.children) {
+				grandchild.parent = child;
+			}
+		}
 
 		this.reset();
 		this.children = [child];
-		this.prefix = prefix.substring(0, pos);
-
-		return true;
+		this.edge = Literal(prefix.substring(0, pos));
 	}
 
 	/**
@@ -112,7 +215,7 @@ class Node<V> {
 		(Subclasses should override this method if they have custom state.)
 	**/
 	private function clone():Node<V> {
-		final other = this.create(this.prefix, this.value);
+		final other = this.create(this.edge, this.value);
 		other.children = this.children;
 		return other;
 	}
@@ -123,7 +226,7 @@ class Node<V> {
 		(Subclasses should override this method if they have custom state.)
 	**/
 	private function reset() {
-		this.prefix = "";
+		this.edge = Literal("");
 		this.value = null;
 		this.children = null;
 	}
