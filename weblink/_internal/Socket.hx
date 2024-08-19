87c3acb01bdbb8a242999ebd81da989a927be4d3
neimanpinchas <neimanpinchas@gmail.com>
2024-08-19 15:11:25 +0300

    stream only on js

--- weblink/_internal/Socket.hx
+++ weblink/_internal/Socket.hx
@@ -1,10 +1,12 @@
 package weblink._internal;
 
 import haxe.io.Bytes;
+#if js
 import js.node.Buffer;
+#end
 #if hl
 import hl.uv.Stream;
-#else
+#elseif js
 // typedef Stream = js.node.net.Socket;
 
 abstract Stream(js.node.net.Socket) {
